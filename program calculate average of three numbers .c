0ae2382e11ff0bba876a9885cfcb88a5f1fcc963
bhagatanushka <banushka615@gmail.com>
2025-04-30 12:44:41 +0530

    Average of three numbers

---
+++ program calculate average of three numbers .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main() 
+ { 
+    int num1,num2,num3,avg;
+    printf("Enter first number");
+    scanf("%d and num1");
+    printf("Enter second number");
+    scanf("%d and num2");
+    printf("Enter third number ");
+    scanf("%d and num3");
+    avg=num1+num2+num3;
+    printf("average of %d and %d is %d num1,num2,num3");
+    avg=(num1,num2,num3);
+    return 0;
+ }  
+  
